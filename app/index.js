de9d3f5da64b0d96cef7167a1e6ec8d5289d72d2
Md. Nayem Ahmed Niloy <71997569+nayemahmedniloy@users.noreply.github.com>
2024-07-10 11:54:12 +0600

    Add files via upload

--- app/index.js
+++ app/index.js
@@ -1,11 +1,24 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { BackHandler, Platform } from 'react-native';
 import { WebView } from 'react-native-webview';
+import { StyleSheet } from 'react-native';
+import React from 'react';
+import { BackHandler, Platform } from 'react-native';
+import { useCallback, useEffect, useState, useRef } from 'react';
+import { Text, View } from 'react-native';
+import Entypo from '@expo/vector-icons/Entypo';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
 
-const MyWebView = () => {
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  // WebView back handler constant
   const webViewRef = useRef(null);
   const [canGoBack, setCanGoBack] = useState(false);
+  // Splash Screen constant
+  const [appIsReady, setAppIsReady] = useState(false);
 
+  // WebView back handling
   const onAndroidBackPress = () => {
     if (canGoBack && webViewRef.current) {
       webViewRef.current.goBack();
@@ -23,13 +36,53 @@ const MyWebView = () => {
     }
   }, [canGoBack]);
 
-  return (
-    <WebView
-      ref={webViewRef}
-      source={{ uri: 'https://aistudio.google.com/app/' }}
-      onNavigationStateChange={navState => setCanGoBack(navState.canGoBack)}
-    />
-  );
-};
+  // Splash screen handling
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync(Entypo.font);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
 
-export default MyWebView;
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <WebView
+        ref={webViewRef}
+        style={styles.webview}
+        source={{ uri: 'https://aistudio.google.com/app/' }}
+        onNavigationStateChange={navState => setCanGoBack(navState.canGoBack)}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+  },
+});
